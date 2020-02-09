fa9790b8aecb7847565b37349e9ac74a88f1742c
Brian Mulhall <brian.e.mulhall@gmail.com>
2020-02-09 11:23:47 -0600

    Step 5.1: Update Task component to add features

--- simple-todos/imports/ui/Task.vue
+++ simple-todos/imports/ui/Task.vue
@@ -1,12 +1,43 @@
 <template>
-  <li>{{ this.task.text }}</li>
+  <li v-bind:class="taskClassName">
+    <button className="delete" @click="deleteThisTask">
+      &times;
+    </button>
+
+    <input
+      type="checkbox"
+      readOnly
+      v-bind:checked="!!this.task.checked"
+      @click="toggleChecked"
+    />
+
+    <span class="text">{{ this.task.text }}</span>
+  </li>
 </template>
 
 <script>
+import { Tasks } from "../api/tasks.js";
+
 export default {
   props: ["task"],
   data() {
     return {};
+  },
+  computed: {
+    taskClassName: function() {
+      return this.task.checked ? "checked" : "";
+    }
+  },
+  methods: {
+    toggleChecked() {
+      // Set the checked property to the opposite of its current value
+      Tasks.update(this.task._id, {
+        $set: { checked: !this.task.checked }
+      });
+    },
+    deleteThisTask() {
+      Tasks.remove(this.task._id);
+    }
   }
 };
 </script>
