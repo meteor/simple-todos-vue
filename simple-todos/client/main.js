c815a73f4dc4fc26fa5a06601be4fb67f0b63550
Brian Mulhall <brian.e.mulhall@gmail.com>
2020-02-15 15:09:02 -0600

    Step 3.4: Add the VueMeteorTracker plugin to the root Vue instance

--- simple-todos/client/main.js
+++ simple-todos/client/main.js
@@ -1,5 +1,8 @@
 import { Meteor } from 'meteor/meteor';
 import Vue from 'vue';
+import VueMeteorTracker from 'vue-meteor-tracker';
+
+Vue.use(VueMeteorTracker);
 
 import App from '../imports/ui/App.vue';
 
